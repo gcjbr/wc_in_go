f90e5312f38e4acab03cea1a52a5341a1747fedd
Gustavo Caetano <souljacker@gmail.com>
2024-05-04 08:41:39 -0300

    Implement count characters with suport to utf

--- wc.go
+++ wc.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -20,7 +21,8 @@ func main() {
 	countLines := flag.Bool("l", false, "Count lines")
 	// countWords := flag.Bool("w", false, "Count words")
 	countBytes := flag.Bool("c", false, "Count bytes")
-	// countChars := flag.Bool("m", false, "Count characters")
+
+	countChars := flag.Bool("m", false, "Count characters")
 	flag.Parse() // Parse the flags
 
 	// Check if args were passed
@@ -41,6 +43,8 @@ func main() {
 	// Ensure the file is closed after the function returns
 	defer file.Close()
 
+	// Check flags
+
 	if countLines != nil && *countLines {
 		lines, err := countLinesFromFile(file)
 
@@ -51,7 +55,20 @@ func main() {
 		println(lines)
 		responses[0] = lines
 	}
+
 	if countBytes != nil && *countBytes {
+		characters, err := countCharBytes(file)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
+		println(characters)
+		responses[2] = characters
+	}
+
+	if countChars != nil && *countChars {
 		characters, err := countCharacters(file)
 
 		if err != nil {
@@ -59,9 +76,32 @@ func main() {
 			os.Exit(0)
 		}
 
-		responses[2] = characters
+		responses[3] = characters
 	}
 
+}
+
+func countCharacters(file *os.File) (int, error) {
+	charCount := 0
+
+	reader := bufio.NewReader(file)
+	var er error
+
+	for {
+		_, _, err := reader.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+
+				er = err
+			}
+
+		}
+		charCount++
+	}
+
+	return charCount, er
 }
 
 func countLinesFromFile(file *os.File) (int, error) {
@@ -81,7 +121,7 @@ func countLinesFromFile(file *os.File) (int, error) {
 
 }
 
-func countCharacters(file *os.File) (int, error) {
+func countCharBytes(file *os.File) (int, error) {
 
 	count := 0
 
